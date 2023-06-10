8da7c09da61219772da8a8ef8b8b2d55a0e61360
claudiney63 <claudineyryan75@gmail.com>
2023-06-10 14:32:13 -0300

    comments add

--- example.py
+++ example.py
@@ -6,16 +6,10 @@ class Estado:
         self.canibais = canibais
         self.barco_esquerda = barco_esquerda
 
-    def __hash__(self):
-        return hash((self.missionarios, self.canibais, self.barco_esquerda))
-
-    def __eq__(self, other):
-        return (self.missionarios, self.canibais, self.barco_esquerda) == (other.missionarios, other.canibais, other.barco_esquerda)
-
-    def __repr__(self):
-        return f"({self.missionarios}, {self.canibais}, {self.barco_esquerda})"
-
     def is_valid(self):
+        """
+            O método is_valid() verifica se o estado é válido (ou seja, se não há mais canibais do que missionários em qualquer margem)
+        """
         if self.missionarios < 0 or self.canibais < 0 or self.missionarios > 3 or self.canibais > 3:
             return False
         if self.missionarios > 0 and self.missionarios < self.canibais:
@@ -25,9 +19,15 @@ class Estado:
         return True
 
     def is_goal(self):
+        """
+        Método is_goal() verifica se é o estado objetivo (todos os missionários e canibais estão na margem oposta). 
+        """
         return self.missionarios == 0 and self.canibais == 0 and not self.barco_esquerda
 
     def successors(self):
+        """
+        O método successors() gera todos os possíveis estados sucessores a partir do estado atual.
+        """
         successors = []
         if self.barco_esquerda:
             for m in range(3):
@@ -46,6 +46,12 @@ class Estado:
         return successors
 
 def encontrar_solucao():
+    """
+    A função encontrar_solucao() utiliza a busca em largura (BFS) para explorar todos os estados possíveis 
+    até encontrar uma solução. Ela mantém uma fila de estados a serem explorados e uma lista de estados visitados 
+    para evitar ciclos. A função retorna uma lista de ações que levam à solução, indicando quantos missionários e 
+    canibais devem ser levados de uma margem para a outra em cada passo.
+    """
     initial_state = Estado(3, 3, True)
     visited = set()
     queue = deque([(initial_state, [])])
@@ -67,6 +73,6 @@ def encontrar_solucao():
 solucao = encontrar_solucao()
 if solucao:
     for i, acao in enumerate(solucao):
-        print(f"Passo {i+1}: Leve {acao[0]} missionários e {acao[1]} canibais para o outro lado.")
+        print(f"Passo {i+1}: Leve {acao[0]} missionarios e {acao[1]} canibais para o outro lado.")
 else:
     print("Não foi encontrada uma solução para o problema.")
