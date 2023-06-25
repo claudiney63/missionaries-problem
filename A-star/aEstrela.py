78dba80e5c488cf45ae817afa29ac6c7cf5e7fc8
claudiney63 <claudineyryan75@gmail.com>
2023-06-25 16:42:25 -0300

    funções comentadas

--- A-star/aEstrela.py
+++ A-star/aEstrela.py
@@ -1,36 +1,43 @@
+# import networkx as nx
+# import matplotlib.pyplot as plt
 from collections import deque
 import Estado
 
-def heuristic(state):
+def heuristica(estado):
     # Função heurística: número de missionários e canibais ainda na margem esquerda
-    return state.missionarios + state.canibais
+    return estado.missionarios + estado.canibais
 
-def a_star_search():
-    initial_state = Estado.Estado(3, 3, True)
-    visited = set()
-    queue = deque([(initial_state, [])])
+def busca_a_estrela():
+    esatdo_inicial = Estado.Estado(3, 3, True)  # Cria um estado inicial com 3 missionários e 3 canibais na margem esquerda
+    visitados = set()  # Conjunto para armazenar os estados visitados
+    queue = deque([(esatdo_inicial, [])])  # Fila para armazenar os estados a serem explorados, com o estado inicial e um caminho vazio
+    caminhos = []  # Lista para armazenar os caminhos percorridos em cada iteração
 
     while queue:
-        queue = deque(sorted(queue, key=lambda x: len(x[1]) + heuristic(x[0])))  # Ordena a fila com base na função heurística e no custo do caminho
-        current_state, path = queue.popleft()
-        if current_state.is_goal():
-            return path
+        queue = deque(sorted(queue, key=lambda x: len(x[1]) + heuristica(x[0])))  # Ordena a fila com base no custo do caminho percorrido até o estado atual e na função heurística
+        esatdo_atual, caminho = queue.popleft()  # Remove o estado com maior prioridade da fila
 
-        visited.add(current_state)
-        successors = current_state.successors()
+        if esatdo_atual.is_goal():  # Verifica se o estado atual é o estado objetivo
+            return caminho, caminhos  # Retorna o caminho da solução e a lista de caminhos percorridos
 
-        for successor, action in successors:
-            if successor not in visited:
-                queue.append((successor, path + [action]))
+        visitados.add(esatdo_atual)  # Adiciona o estado atual aos estados visitados
+        sucessores = esatdo_atual.successors()  # Gera os estados sucessores do estado atual
 
-    return None
+        for sucessor, acao in sucessores:
+            if sucessor not in visitados:  # Verifica se o sucessor já foi visitado
+                queue.append((sucessor, caminho + [acao]))  # Adiciona o sucessor à fila com o caminho atualizado
 
-print("A* Search:")
-solucao_astar = a_star_search()
-if solucao_astar:
+        caminhos.append(caminho + [esatdo_atual])  # Adiciona o caminho percorrido até o estado atual à lista de caminhos
+
+    return None, caminhos  # Retorna None para indicar que não foi encontrada uma solução e a lista de caminhos percorridos
+
+print("Busca A*: ")
+solucao_a_estrela, caminhos = busca_a_estrela()
+
+if solucao_a_estrela:
     lado = False
-    for i, acao in enumerate(solucao_astar):
+    for i, acao in enumerate(solucao_a_estrela):
         lado = not lado
-        print(f"Passo {i+1}: Leve {acao[0]} missionarios e {acao[1]} canibais para o lado {'direito' if lado == True else 'esquerdo'}.")
+        print(f"Passo {i+1}: Leve {acao[0]} missionarios e {acao[1]} canibais para o lado {'direito' if lado else 'esquerdo'}.")
 else:
     print("Não foi encontrada uma solução para o problema usando A* Search.")
